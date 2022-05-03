5857b9eca6e5e4b712607d84543496ff4d19e5fb
BenoitThaon <benoit.thaon@gmail.com>
2022-05-03 11:33:32 +0200

    feature-fixture-userFixture Ok création d'un user ROLE_USER et un ROLE_ADMIN

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -16,21 +16,37 @@ class UserFixture extends Fixture
         // TODO récupérer faker
         $faker = Factory::create('fr-FR');
 
-        // TODO boucler pour en créer 20
-        for ($i=0; $i < 20 ; $i++) {
+        // TODO créer un nouvel utilisateur user
+        $newUser = new User();
+        // TODO renseigner toutes les propriétés
+        $newUser->setUserLastname($faker->unique()->lastName())
+                ->setUserFirstname($faker->firstName())
+                ->setUserAdress($faker->address())
+                ->setUserBirthdate(new DateTime())
+                ->setUserPassword('user')// TODO hasher le password
+                ->setUserRole('ROLE_USER')
+                ->setUserMail('user@user.com')
+                ->setUserPhone($faker->phoneNumber())
+                ->setUserCreatedAt(new DateTime());
+        // TODO persist
+        $manager->persist($newUser);
 
-            // TODO créer un nouvel utilisateur
-            $newUser = new User();
-            // TODO renseigner toutes les propriétés
-            $newUser->setUserLastname($faker->unique()->lastName())
-                    ->setUserFirstname($faker->firstName())
-                    ->setUserAdress($faker->address())
-                    ->setUserBirthdate(new DateTime());
+        // TODO créer un nouvel utilisateur admin
+        $newUserAdmin = new User();
+        // TODO renseigner toutes les propriétés
+        $newUserAdmin->setUserLastname($faker->unique()->lastName())
+                ->setUserFirstname($faker->firstName())
+                ->setUserAdress($faker->address())
+                ->setUserBirthdate(new DateTime())
+                ->setUserPassword('admin')// TODO hasher le password
+                ->setUserRole('ROLE_ADMIN')
+                ->setUserMail('admin@admin.com')
+                ->setUserPhone($faker->phoneNumber())
+                ->setUserCreatedAt(new DateTime());
+        // TODO persist
+        $manager->persist($newUserAdmin);
+
         
-            //? hasher le mot de passe
-            // TODO persist
-        }
-
         $manager->flush();
     }
 }
