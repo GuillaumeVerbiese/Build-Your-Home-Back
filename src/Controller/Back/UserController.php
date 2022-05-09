6332e87b61c1fd5a333f87d434475453dd5db531
BenoitThaon <benoit.thaon@gmail.com>
2022-05-09 10:11:54 +0200

    feature-crud-User with try/catch, flashMessage and css

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use App\Form\UserEditType;
 use App\Repository\UserRepository;
 use DateTime;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -45,7 +46,12 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($user);
-            $entityManager->flush();
+            try {
+                $entityManager->flush();
+            }catch(UniqueConstraintViolationException $e){
+                $this->addFlash('danger','Cette adresse email est déjà lié à un autre compte !');
+                return $this->redirectToRoute('app_back_user_new', [], Response::HTTP_FOUND);
+            }
 
             return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -57,34 +63,40 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="app_back_user_show", methods={"GET"})
+     * @Route("/{id}", name="app_back_user_show", methods={"GET"}, requirements={"id":"\d+"})
      */
-    public function show(User $user): Response
+    public function show(User $user = null): Response
     {
+        if ($user === null) {
+            $this->addFlash('danger',"Cet utilisateur n'existe pas !");
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+        }
         return $this->render('back/user/show.html.twig', [
             'user' => $user,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="app_back_user_edit", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="app_back_user_edit", methods={"GET", "POST"}, requirements={"id":"\d+"})
      */
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user = null, EntityManagerInterface $entityManager): Response
     {
+        if ($user === null) {
+            $this->addFlash('danger',"Cet utilisateur n'existe pas !");
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+        }
         $form = $this->createForm(UserEditType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
             $user->setUpdatedAt(new DateTime());
-            // TODO mettre en place un try/catch pour erreur 500 email -> unique
-            // try {
+            try {
                 $entityManager->flush();
-            // }catch(Exception $e){
-            //     $this->addFlash('danger','Cette adresse email est déjà lié à un autre compte !');
-            //     $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
-
-            // }
+            }catch(UniqueConstraintViolationException $e){
+                $this->addFlash('danger','Cette adresse email est déjà lié à un autre compte !');
+                return $this->redirectToRoute('app_back_user_edit', ["id"=>$user->getId()], Response::HTTP_FOUND);
+            }
 
             return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -96,11 +108,51 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="app_back_user_delete", methods={"POST"})
+     * @Route("/{id}", name="app_back_user_delete", methods={"POST"}, requirements={"id":"\d+"})
      */
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, User $user = null, EntityManagerInterface $entityManager): Response
     {
+        if ($user === null) {
+            $this->addFlash('danger',"Cet utilisateur n'existe pas !");
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+        }
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            // On récupére ses favoris
+        $userFavorites = $user->getFavorites();
+        // Si il y a des favories
+        if ($userFavorites !== null) {
+            // On boucle sur le tableau pour les supprimer
+            foreach ($userFavorites as $favorite) {
+                $entityManager->remove($favorite);
+            }
+        }
+        // On récupére ses commandes
+        $userOrders = $user->getOrders();
+        // Si il y a des commandes
+        if ($userOrders !== null) {
+        
+            // On boucle sur le tableau pour les supprimer
+            foreach ($userOrders as $order) {
+                // Avant de supprimer la commande il faut supprimer leurs orderlist
+                $orderOrderlists = $order->getOrderlists();
+                // On boucle sur le tableau pour les supprimer
+                foreach ($orderOrderlists as $orderlist) {
+                    $entityManager->remove($orderlist);
+                }
+                // Puis on supprime la commande
+                $entityManager->remove($order);
+            }
+        }
+        // On récupére ses commentaires
+        $userComments = $user->getComments();
+        // Si il y a des commandes
+        if ($userComments !== null) {
+        
+            // On boucle sur le tableau pour supprimer leur utilisateur associé
+            foreach ($userComments as $comment) {
+                $comment->setUser(null);
+            }
+        }
             $entityManager->remove($user);
             $entityManager->flush();
         }
