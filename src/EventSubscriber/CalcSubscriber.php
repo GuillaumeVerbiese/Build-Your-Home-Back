d6c197463f201759ee76418bccabf3df46b918f3
BenoitThaon <benoit.thaon@gmail.com>
2022-05-18 15:10:35 +0200

    feature-backoffice-added display of numb of order by status

--- src/EventSubscriber/CalcSubscriber.php
+++ src/EventSubscriber/CalcSubscriber.php
@@ -3,17 +3,20 @@
 namespace App\EventSubscriber;
 
 use App\Repository\ArticleRepository;
+use App\Repository\OrderRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Twig\Environment;
 
 class CalcSubscriber implements EventSubscriberInterface
 {
+    private $orderRepository;
     private $articleRepository;
     private $twig;
 
-    public function __construct(ArticleRepository $articleRepository, Environment $twig)
+    public function __construct(ArticleRepository $articleRepository, Environment $twig, OrderRepository $orderRepository)
     {
+        $this->orderRepository = $orderRepository;
         $this->articleRepository = $articleRepository;
         $this->twig = $twig;
     }
@@ -22,8 +25,28 @@ class CalcSubscriber implements EventSubscriberInterface
     {
         // On récupére le tableau d'articles à commander
         $articlesList = $this->articleRepository->findEmptyStock();
-        // On le donne à twig
+        // On donne la longueur du tableau à twig
         $this->twig->addGlobal("nbOfArticlesToOrder", count($articlesList));
+
+        // On récupére le tableau de commandes en attente
+        $pendingOrders = $this->orderRepository->findBy(["status"=>0]);
+        // On donne la longueur du tableau à twig
+        $this->twig->addGlobal("nbOfPendingOrders", count($pendingOrders));
+
+        // On récupére le tableau de commandes en attente
+        $validateOrders = $this->orderRepository->findBy(["status"=>1]);
+        // On donne la longueur du tableau à twig
+        $this->twig->addGlobal("nbOfValidateOrders", count($validateOrders));
+
+        // On récupére le tableau de commandes en attente
+        $pendingStockOrders = $this->orderRepository->findBy(["status"=>2]);
+        // On donne la longueur du tableau à twig
+        $this->twig->addGlobal("nbOfPendingStockOrders", count($pendingStockOrders));
+
+        // On récupére le tableau de commandes en attente
+        $ordersShipped = $this->orderRepository->findBy(["status"=>3]);
+        // On donne la longueur du tableau à twig
+        $this->twig->addGlobal("nbOfOrdersShipped", count($ordersShipped));
     }
 
     public static function getSubscribedEvents(): array
