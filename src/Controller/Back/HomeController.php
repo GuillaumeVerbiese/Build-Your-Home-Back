fe92eb5458b80e5df8ea418e7a0b87ce3cf1e4fb
BenoitThaon <benoit.thaon@gmail.com>
2022-05-20 10:01:43 +0200

    redirect to login page when you want to go to home if user is not logged in

--- src/Controller/Back/HomeController.php
+++ src/Controller/Back/HomeController.php
@@ -13,8 +13,12 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('back/home/index.html.twig', [
+        if ($this->getUser()) {
+            return $this->render('back/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+        }
+
+        return $this->redirectToRoute('app_login');
     }
 }
